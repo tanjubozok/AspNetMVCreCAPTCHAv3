1bf33a87a93ca5140cc5edd8ddecc9abb9a19222
Tanju BOZOK <tanjubozok@hotmail.com>
2021-12-31 01:01:52 +0400

    Add project files.

---
+++ AspNetMVCreCAPTCHAv3/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using AspNetMVCreCAPTCHAv3.Models;
+using System.Web.Mvc;
+
+namespace AspNetMVCreCAPTCHAv3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Create()
+        {
+            return View(new RegisterModel());
+        }
+
+        [HttpPost]
+        public ActionResult Create(RegisterModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Success");
+            }
+            return View(model);
+        }
+
+        public ActionResult Success()
+        {
+            return View();
+        }
+    }
+}
---
+++ AspNetMVCreCAPTCHAv3/Models/RegisterModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AspNetMVCreCAPTCHAv3.Models
+{
+    public class RegisterModel
+    {
+        [Required]
+        public string FullName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
